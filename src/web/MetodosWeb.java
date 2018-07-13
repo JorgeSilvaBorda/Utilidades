265a44a45cdb62e4eaa868b611feac56eba0c47c
Jorge <jorge.silva8686@gmail.com>
2018-07-13 15:10:42 -0400

    Clase MetodosWeb

---
+++ src/web/MetodosWeb.java
@@ -0,0 +1,32 @@
+package web;
+
+import com.Parametros;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Clase con métodos para acceso y trabajo WEB.
+ * @author Jorge Silva Borda
+ */
+public class MetodosWeb {
+    
+    private static String getWeb(String ruta){
+	try {
+	    URL url = new URL(ruta);
+	    HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+	    BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+	    StringBuilder builder = new StringBuilder();
+	    String linea;
+	    while((linea = br.readLine()) != null){
+		builder.append(linea).append(Parametros.SEP);
+	    }
+	    return builder.toString();
+	} catch (IOException ex) {
+	    System.out.println("No se pudo conectar con la ruta." + Parametros.SEP + ex);
+	    return "";
+	}
+    }
+}
